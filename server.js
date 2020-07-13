5534879f40fb2ee298bbede45440e6f158252ef9
qt.luu <quocthinhluu97@gmail.com>
2020-07-13 11:05:15 +0700

    Add author search feature

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
 
 
 app.set('view engine', 'ejs')
@@ -13,7 +15,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,5 +27,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db = mongoose.connection
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
